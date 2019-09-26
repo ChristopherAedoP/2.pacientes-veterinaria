4f22570c19911b6ae3eac2af874454f9b77e3d0b
Christopher Aedo <Christopher.aedo.p@gmail.com>
2019-09-25 22:27:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import './bootstrap.min.css';
+import Header from './Components/Header';
+import NuevaCita from './Components/NuevaCita';
+import ListaCitas from './Components/ListaCitas';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {  
+    citas : []
+  }
+  //cuando la aplicacion carga
+  componentDidMount(){
+    const citasLS = localStorage.getItem('citas')
+    if (citasLS) {
+      this.setState({
+        citas: JSON.parse(citasLS)
+      })
+    }
+  }
+  componentDidUpdate(){
+    localStorage.setItem('citas',JSON.stringify(this.state.citas))
+  }
 
+  crearNuevaCita = datos => {
+    //copair state actual
+    const citas = [...this.state.citas,datos];
+    this.setState({
+      citas:citas
+    })
+  }
+  eliminarCita = id => {
+    //copiar state
+    const citasActuales = [...this.state.citas]
+    //elimiar id de la lista actual de citas.
+    const citas = citasActuales.filter(cita => cita.id !== id)
+
+    this.setState({
+      citas
+    })
+  }
+  render(){
+    return ( 
+      <div className="container">
+        <Header titulo='Administracion Pacientes Veterinaria' />
+
+        <div className="row">
+          <div className="col-md-10 mx-auto">
+            <NuevaCita crearNuevaCita={this.crearNuevaCita}/>
+          </div>
+
+          <div className="mt-5 col-md-10 mx-auto">
+            <ListaCitas 
+              citas={this.state.citas} 
+              eliminarCita={this.eliminarCita}
+            />
+          </div>
+
+        </div>
+      </div>
+     );
+  }
+};
+ 
 export default App;
+
+
